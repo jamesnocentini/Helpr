8dc3889540029276045a9ec27cc1923d735124ea
Vladimir Petrov <vlad@trendsettr.co.uk>
2014-08-06 17:24:22 +0100

    Products

---
+++ iOS/Helpr/Helpr/Category.h
@@ -0,0 +1,13 @@
+//
+//  Category.h
+//  Helpr
+//
+//  Created by Vladimir Petrov on 06/08/2014.
+//  Copyright (c) 2014 James Nocentini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Category : NSObject
+
+@end
---
+++ iOS/Helpr/Helpr/Category.m
@@ -0,0 +1,13 @@
+//
+//  Category.m
+//  Helpr
+//
+//  Created by Vladimir Petrov on 06/08/2014.
+//  Copyright (c) 2014 James Nocentini. All rights reserved.
+//
+
+#import "Category.h"
+
+@implementation Category
+
+@end
